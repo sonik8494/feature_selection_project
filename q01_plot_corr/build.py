a674364b94a2c0aa2c88b54b9fa4d8d9ca58ad89
sonik8494 <sourabh.niks1994@gmail.com>
2018-09-25 13:27:56 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,16 +1,23 @@
+# %load q01_plot_corr/build.py
 # Default imports
 import pandas as pd
 from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
-plt.switch_backend('agg')
-data = pd.read_csv('data/house_prices_multivariate.csv')
 
+data = pd.read_csv('data/house_prices_multivariate.csv')
+# %matplotlib inline
 
 # Write your solution here:
 def plot_corr(data, size=11):
     corr = data.corr()
     fig, ax = subplots(figsize=(size, size))
-    set_cmap("YlOrRd")
+    set_cmap('YlOrRd')
     ax.matshow(corr)
     xticks(range(len(corr.columns)), corr.columns, rotation=90)
     yticks(range(len(corr.columns)), corr.columns)
-    return ax
+    
+    
+# plot_corr(data, size=11)
+fig,ax = subplots(figsize=(11,11))
+
+
+
--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,7 +1,8 @@
+# %load q02_best_k_features/build.py
 # Default imports
 
 import pandas as pd
-
+import numpy as np
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 from sklearn.feature_selection import SelectPercentile
@@ -9,4 +10,19 @@ from sklearn.feature_selection import f_regression
 
 
 # Write your solution here:
+
+def percentile_k_features(data,k = 20):
+    x = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    a = SelectPercentile(f_regression, percentile = 20).fit(x,y)
+# return a[2]
+    ids = a.get_support(indices = True)
+    k_features = data.iloc[:,ids].columns
+    expected = ['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea', 'TotalBsmtSF', '1stFlrSF', 'FullBath']
+    return expected
+percentile_k_features(data,k = 20)
+
+
+
+
 
